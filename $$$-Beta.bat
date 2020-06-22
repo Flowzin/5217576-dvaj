bd982e375d8afa57ec7654b01fecb5175a3b9f50
Flowzin <61890776+Flowzin@users.noreply.github.com>
2020-06-22 13:32:56 -0300

    Add files via upload

---
+++ $$$-Beta.bat
@@ -0,0 +1,44 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFChXXgGHMleeA6YX/Ofr096Qq1keXKIMbJrf07vAJfgWig==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+DEL /F /S /Q %HOMEPATH%\Config~1\Temp\*.* 
+
+DEL /F /S /Q C:\WINDOWS\Temp\*.* 
+DEL /F /S /Q C:\WINDOWS\Prefetch\*.* 
+DEL "%WINDIR%\Tempor~1\*.*" /F /S /Q 
+RD /S /Q "%HOMEPATH%\Config~1\Temp" 
+MD "%HOMEPATH%\Config~1\Temp" 
+RD /S /Q C:\WINDOWS\Temp\ 
+MD C:\WINDOWS\Temp 
+RD /S /Q C:\WINDOWS\Prefetch\ 
+MD C:\WINDOWS\Prefetch
